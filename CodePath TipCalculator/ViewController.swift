10230c69f1e78b2deb878689d8195bbf6a9e2aa1
Nathan Miranda <nmiranda4@fordham.edu>
2015-12-25 15:27:39 -0500

    Initial Push
    Added code and Xcode files

---
+++ CodePath TipCalculator/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  CodePath TipCalculator
+//
+//  Created by Nathan Miranda on 12/24/15.
+//  Copyright © 2015 Miraen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.2, 0.25]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
+
